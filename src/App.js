efe5d309be0332e357fd84e729b1f0ed6a3fec97
saikumar <saikumar@digisprint.com>
2024-03-20 23:47:16 +0530

    created start rating app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  const [input, setInput] = useState(0);
+
+  const handleChange = (e) => {
+    // here we need to chek the truty value  for my ref
+    setInput(+(Boolean(e.target.value) ? e.target.value : 0));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Rating</h1>
+      {new Array(input).fill().map((start, index) => {
+        //index is start from 0
+        const currentIndex = index + 1 ;
+
+        return (
+          <span
+            key={index}
+            style={{
+              color: currentIndex <= (hover || rating) ? "red" : "green",
+              cursor: "pointer",
+              fontSize: "50px",
+            }}
+            onClick={() => setRating(currentIndex)}
+            onMouseEnter={() => setHover(currentIndex)}
+            onMouseLeave={() => setHover(null)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
+      <div>
+        <h3>Enter Number</h3>
+        <input type="number" value={input} onChange={handleChange} />
+      </div>
     </div>
   );
 }
